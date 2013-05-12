9000f0fad4f7411c29f8edc3eacc26f1962fabf4
Chaoxu Tong <blackchaoxu@gmail.com>
2013-05-12 15:47:11 -0400

    modified segtree

--- segtree.cpp
+++ segtree.cpp
@@ -2,36 +2,41 @@
 using namespace std;
 typedef vector<int> vi;
 
+// use [val] to store intrinsic values
+// use [aux] to store update info
+// this verison implements length of covered intervals
 struct segtree {
-  vi count, len;
+  vi val, aux;
   int n;
 
   segtree(int N) {
     for (n = 1; n < N; n *= 2);
-    count.resize(2*n-1);
-    len.resize(2*n-1);
+    val.resize(2*n-1);
+    aux.resize(2*n-1);
   }
 
-  void modify(int l, int r, int d) { modify(l, r, d, 0, 0, n);}
-  void modify(int l, int r, int d, int u, int L, int R) {
+  void update(int l, int r, int d) { update(l, r, d, 0, 0, n);}
+  void update(int l, int r, int d, int u, int L, int R) {
     if (l >= R || r <= L || l >= r) return;
-    if (l <= L && r >= R) count[u] += d;
+    if (l <= L && r >= R) val[u] += d;
     else {
       int M = (L+R)/2;
-      modify(l, r, d, 2*u+1, L, M);
-      modify(l, r, d, 2*u+2, M, R);
+      update(l, r, d, 2*u+1, L, M);
+      update(l, r, d, 2*u+2, M, R);
     }
 
-    if (count[u] > 0) len[u] = R-L;
-    else len[u] = R-L>1 ? len[2*u+1] + len[2*u+2] : 0;
+    if (val[u] > 0) aux[u] = R-L;
+    else aux[u] = R-L>1 ? aux[2*u+1] + aux[2*u+2] : 0;
   }
 
   // get the length of covered intervals in [l,r)
-  int getlen(int l, int r) { return getlen(l, r, 0, 0, n);}
-  int getlen(int l, int r, int u, int L, int R) {
+  int query(int l, int r) { return query(l, r, 0, 0, n);}
+  int query(int l, int r, int u, int L, int R) {
     if (l >= R || r <= L || l >= r) return 0;
-    if (l <= L && r >= R) return len[u];
+    if (l <= L && r >= R) return aux[u];
+
+    if (val[u] > 0) return min(r, R) - max(l, L);
     int M = (L+R)/2;
-    return getlen(l, r, u*2+1, L, M) + getlen(l, r, u*2+2, M, R);
+    return query(l, r, u*2+1, L, M) + query(l, r, u*2+2, M, R);
   }
 };
