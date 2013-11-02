7a031a368d3ffa0c39a4b00485c576457db2a0da
Chaoxu Tong <blackchaoxu@gmail.com>
2013-11-02 17:57:49 -0400

    added chinese reminder theorem

--- number.cpp
+++ number.cpp
@@ -1,9 +1,13 @@
-int euclid(int a, int b, int& x, int& y) {
-  if (b == 0) {
-    x = 1; y = 0;
-    return a;
-  }
-  int res = euclid(b, a%b, x, y);
-  int t = x; x = y; y = t - (a/b) * y;
-  return res;
+#include "header.cpp"
+
+ll bezout(ll a, ll b, ll& x, ll& y) {
+  if (b == 0) return x=1,y=0,a;
+  ll res = bezout(b, a%b, x, y), t = x;
+  return x = y, y = t - (a/b) * y, res;
+}
+
+ll crt(ll p, ll a, ll q, ll b) {
+  ll m = p*q, x, y;
+  bezout(p, q, x, y);
+  return mod(mod(a*y,m)*q+mod(b*x,m)*p,m);
 }
