3b51c92fbf13ad7e3af1f24c86db9ba497bcc3b7
Chaoxu Tong <blackchaoxu@gmail.com>
2013-11-03 14:46:05 -0500

    added BIT and RMQ

--- segtree.cpp
+++ segtree.cpp
@@ -3,7 +3,6 @@
 // use [val] to store intrinsic values
 // use [aux] to store delta info
 // this verison implements length of covered intervals
-// Can be implemented for BIT, RMQ
 struct segtree {
   vi val, aux;
   int n;
@@ -39,12 +38,46 @@ struct segtree {
   }
 };
 
-struct bit {
+struct RMQ {
+  int n;
+  vi val, aux;
+
+  RMQ(int N) {
+    for (n = 1; n < N; n *= 2);
+    val.resize(2*n-1);
+    aux.resize(2*n-1);
+  }
+
+  void update(int i, int x) { delta(i, x-query(i));}
+  void delta(int i, int d) { delta(i, i+1, d);}
+  void delta(int l, int r, int d) { delta(l, r, d, 0, 0, n);}
+  void delta(int l, int r, int d, int u, int L, int R) {
+    if (l >= R || r <= L || l >= r) return;
+    if (l <= L && r >= R) val[u] += d;
+    else {
+      int M = (L+R)/2;
+      delta(l, r, d, 2*u+1, L, M);
+      delta(l, r, d, 2*u+2, M, R);
+    }
+    aux[u] = val[u] + (R-L>1 ? min(aux[2*u+1], aux[2*u+2]) : 0);
+  }
+
+  int query(int i) { return query(i, i+1);}
+  int query(int l, int r) { return query(l, r, 0, 0, n);}
+  int query(int l, int r, int u, int L, int R) {
+    if (l >= R || r <= L || l >= r) return INF;
+    if (l <= L && r >= R) return aux[u];
+    int M = (L+R)/2;
+    return min(query(l, r, u*2+1, L, M), query(l, r, u*2+2, M, R)) + val[u];
+  }
+};
+
+struct BIT {
   int n;
   // val[u] is delta to all nodes in u, aux[u] is sum of subtree at u
   vi val, aux;
 
-  bit(int N) {
+  BIT(int N) {
     for (n = 1; n < N; n *= 2);
     val.resize(2*n-1);
     aux.resize(2*n-1);
