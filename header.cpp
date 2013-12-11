d9183d7fe23507f8fe0d1494411e88818a2c0776
Chaoxu Tong <blackchaoxu@gmail.com>
2013-12-11 17:04:17 -0500

    added split

--- header.cpp
+++ header.cpp
@@ -14,8 +14,8 @@
 #define ALL(c) (c).begin(), (c).end()
 #define MSET(c, v) memset(c, v, sizeof(c))
 #define TRACE(x...) x
-#define PRINT(x...) TRACE(printf(x))
-#define WATCH(x) TRACE({auto _ = (x); cout << #x << " = " << _ << endl;})
+#define PRINT(x...) TRACE(fprintf(stderr, x))
+#define WATCH(x) TRACE(auto _VAL = (x); cerr << #x << " = " << _VAL << endl;)
 using namespace std;
 typedef vector<int> vi;
 typedef long long ll;
@@ -40,3 +40,10 @@ template<class C> inline void chmin(C& x, const C& a) { if (x > a) x = a;}
 template<class C> string to_string(const C& x) {stringstream ss;ss << x;return ss.str();}
 template<class C> C from_string(const string& s) {stringstream ss(s);C x;ss >> x;return x;}
 template<class C> inline C mod(C a, C b) { return (a%b+b)%b;}
+vector<string> split(const string& line, char delim = ' ') {
+  vector<string> ret;
+  stringstream ss(line);
+  string s;
+  while(getline(ss, s, delim)) ret.PB(s);
+  return ret;
+}
