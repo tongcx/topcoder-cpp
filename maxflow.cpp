62039ed566dea7eb47dd3c5f6744f15dbe34410a
Chaoxu Tong <blackchaoxu@gmail.com>
2014-05-10 22:41:17 -0700

    commented maxflow and mcmf

--- maxflow.cpp
+++ maxflow.cpp
@@ -1,59 +1,78 @@
 #include "header.cpp"
 
-// Max Flow! - Dinic O(n^2 * m) incremental
-struct MaxFlow {
-  int n;    // n is number of nodes
-  vi dest, cap, flow, d, curAdj;// use SZ(dest) as nar (including back arcs)
-  vector<vi> adj;
+/**
+ * Max Flow using Dinic O(n^2 * m), it's incremental.
+ * Use flow to retrieve flow value.
+ */
+class MaxFlow {
+  public:
+    /**
+     * Initialize a graph with n nodes
+     */
+    MaxFlow(int n):n(n), adj(n) {}
 
-  MaxFlow(int n = 0):n(n) {adj.resize(n);}
-  int inv(int a) { return a ^ 0x1; }
-  int orig(int a) { return dest[inv(a)]; }
-  int capres(int a) { return cap[a] - flow[a]; }
-  int arc(int i, int j, int u) {
-    dest.PB(j); adj[i].PB(SZ(dest)-1); cap.PB(u);
-    dest.PB(i); adj[j].PB(SZ(dest)-1); cap.PB(0);
-    return SZ(dest)-2;
-  }
+    /**
+     * Add arc from i to j with capacity u. Return
+     * id of this arc.
+     */
+    int arc(int i, int j, int u) {
+      dest.PB(j); adj[i].PB(SZ(dest)-1); cap.PB(u);
+      dest.PB(i); adj[j].PB(SZ(dest)-1); cap.PB(0);
+      return SZ(dest)-2;
+    }
 
-  bool MFbfs(int ini, int end) {
-    d = vi(n, INF); curAdj = vi(n); d[ini] = 0;
-    queue<int> Q; Q.push(ini);
-    while (!Q.empty()) {
-      int u = Q.front(); Q.pop();
-      for(auto i : adj[u]) {
-        int v = dest[i];
-        if (capres(i) > 0 && d[v] == INF) {
-          d[v] = d[u] + 1; Q.push(v);
+    /**
+     * Return maxflow from ini to end
+     */
+    int maxflow(int ini, int end) {
+      flow = vi(SZ(dest));
+      int F = 0;
+      while (MFbfs(ini, end)) while (true) {
+        int tmp = MFdfs(ini, end, INF);
+        F += tmp;
+        if (tmp == 0) break;
+      }
+      return F;
+    }
+
+    vi flow;
+
+  private:
+    int n;    // n is number of nodes
+    vi dest, cap, d, curAdj;// use SZ(dest) as nar (including back arcs)
+    vector<vi> adj;
+
+    int inv(int a) { return a ^ 0x1; }
+    int orig(int a) { return dest[inv(a)]; }
+    int capres(int a) { return cap[a] - flow[a]; }
+
+    bool MFbfs(int ini, int end) {
+      d = vi(n, INF); curAdj = vi(n); d[ini] = 0;
+      queue<int> Q; Q.push(ini);
+      while (!Q.empty()) {
+        int u = Q.front(); Q.pop();
+        for(auto i : adj[u]) {
+          int v = dest[i];
+          if (capres(i) > 0 && d[v] == INF) {
+            d[v] = d[u] + 1; Q.push(v);
+          }
         }
       }
+      return d[end] != INF;
     }
-    return d[end] != INF;
-  }
 
-  int MFdfs(int u, int t, int f) {
-    if (u == t) return f;
-    for(int &i = curAdj[u]; i < SZ(adj[u]); ++i) {
-      int ar = adj[u][i], v = dest[ar];
-      if (d[v] != d[u]+1 || capres(ar) == 0) continue;
-      int tmpF = MFdfs(v, t, min(f, capres(ar)));
-      if (tmpF) {
-        flow[ar] += tmpF;
-        flow[inv(ar)] -= tmpF;
-        return tmpF;
+    int MFdfs(int u, int t, int f) {
+      if (u == t) return f;
+      for(int &i = curAdj[u]; i < SZ(adj[u]); ++i) {
+        int ar = adj[u][i], v = dest[ar];
+        if (d[v] != d[u]+1 || capres(ar) == 0) continue;
+        int tmpF = MFdfs(v, t, min(f, capres(ar)));
+        if (tmpF) {
+          flow[ar] += tmpF;
+          flow[inv(ar)] -= tmpF;
+          return tmpF;
+        }
       }
+      return 0;
     }
-    return 0;
-  }
-
-  int maxflow(int ini, int end) {
-    flow = vi(SZ(dest));
-    int F = 0;
-    while (MFbfs(ini, end)) while (true) {
-      int tmp = MFdfs(ini, end, INF);
-      F += tmp;
-      if (tmp == 0) break;
-    }
-    return F;
-  }
 };
--- mcmf.cpp
+++ mcmf.cpp
@@ -3,56 +3,82 @@
 typedef int cost_type;
 const cost_type CINF = 0x3F3F3F3F;
 
-// MinCostMaxFlow - O(n * m^2 * log U) with SPFA
-struct MCMF {
-  int n, delta, F;    // n is number of nodes
-  cost_type C;
-  vector<cost_type> cost;
-  vi dest, cap, flow;  // use SZ(dest) as nar (including back arcs)
-  vector<vi> adj;
+/**
+ * MinCostMaxFlow - O(n * m^2 * log U) with SPFA.
+ * When cost is double, change cost_type and CINF.
+ *
+ * After execution, access F to know the flow amount.
+ * Access C to know the total cost.
+ * Access flow for detailed flow value.
+ */
+class MCMF {
+  public:
+    int F;
+    cost_type C;
+    vi flow;
 
-  MCMF(int n):n(n) {adj.resize(n);}
-  int inv(int a) { return a ^ 0x1; }
-  int orig(int a) { return dest[inv(a)]; }
-  int capres(int a) { return cap[a] - flow[a]; }
-  int arc(int i, int j, int u, cost_type c = 0) {
-    dest.PB(j); adj[i].PB(SZ(dest)-1); cap.PB(u); cost.PB(c);
-    dest.PB(i); adj[j].PB(SZ(dest)-1); cap.PB(0); cost.PB(-c);
-    return SZ(dest)-2;
-  }
+    /**
+     * Initialize a graph with n nodes
+     */
+    MCMF(int n):n(n), adj(n) {}
 
-  bool spfa(int ini, int end) {
-    vi ent(n, -1);
-    vector<cost_type> dist(n, CINF);
-    vector<bool> mark(n); queue<int> q;
+    /**
+     * Add arc from i to j with capacity u and
+     * cost c (default to 0). Return the id of
+     * this arc.
+     */
+    int arc(int i, int j, int u, cost_type c = 0) {
+      dest.PB(j); adj[i].PB(SZ(dest)-1); cap.PB(u); cost.PB(c);
+      dest.PB(i); adj[j].PB(SZ(dest)-1); cap.PB(0); cost.PB(-c);
+      return SZ(dest)-2;
+    }
 
-    dist[ini] = 0; q.push(ini); mark[ini] = true;
-    while(!q.empty()) {
-      int u = q.front(); q.pop(); mark[u] = false;
-      for(auto a : adj[u]) {
-        int v = dest[a];
-        if (capres(a) >= delta && dist[v] > dist[u]+cost[a]) {
-          dist[v] = dist[u]+cost[a];
-          ent[v] = a;
-          if (!mark[v]) { q.push(v); mark[v] = true; }
+    /**
+     * Compute min cost max flow from ini to end.
+     * Return total cost.
+     */
+    cost_type mcmf(int ini, int end) {
+      flow = vi(SZ(dest)); F = C = 0; int U = 0;
+      REP(a, SZ(dest)) U = max(U, capres(a));
+      for (delta = U; delta > 0; delta /= 2) while (spfa(ini, end));
+      return C;
+    }
+
+  private:
+    int n, delta;    // n is number of nodes
+    vector<cost_type> cost;
+    vi dest, cap;  // use SZ(dest) as nar (including back arcs)
+    vector<vi> adj;
+
+    int inv(int a) { return a ^ 0x1; }
+    int orig(int a) { return dest[inv(a)]; }
+    int capres(int a) { return cap[a] - flow[a]; }
+
+    bool spfa(int ini, int end) {
+      vi ent(n, -1);
+      vector<cost_type> dist(n, CINF);
+      vector<bool> mark(n); queue<int> q;
+
+      dist[ini] = 0; q.push(ini); mark[ini] = true;
+      while(!q.empty()) {
+        int u = q.front(); q.pop(); mark[u] = false;
+        for(auto a : adj[u]) {
+          int v = dest[a];
+          if (capres(a) >= delta && dist[v] > dist[u]+cost[a]) {
+            dist[v] = dist[u]+cost[a];
+            ent[v] = a;
+            if (!mark[v]) { q.push(v); mark[v] = true; }
+          }
         }
       }
-    }
 
-    if (ent[end] == -1) return false;
-    for (int a = ent[end]; a != -1; a = ent[orig(a)]) {
-      C += delta * cost[a];
-      flow[a] += delta;
-      flow[inv(a)] -= delta;
+      if (ent[end] == -1) return false;
+      for (int a = ent[end]; a != -1; a = ent[orig(a)]) {
+        C += delta * cost[a];
+        flow[a] += delta;
+        flow[inv(a)] -= delta;
+      }
+      F += delta;
+      return true;
     }
-    F += delta;
-    return true;
-  }
-
-  cost_type mcmf(int ini, int end) {
-    flow = vi(SZ(dest)); F = C = 0; int U = 0;
-    REP(a, SZ(dest)) U = max(U, capres(a));
-    for (delta = U; delta > 0; delta /= 2) while (spfa(ini, end));
-    return C;
-  }
 };
