c0caacff548beb19cfc019898805bba4997ff5d5
Chaoxu Tong <blackchaoxu@gmail.com>
2015-03-01 11:58:47 -0500

    small change

--- linalg/test_matrix.cpp
+++ linalg/test_matrix.cpp
@@ -7,7 +7,5 @@ TEST(matrix, basic) {
       ffvec(7, {3, 2, 6, 0}) );
 
   EXPECT_EQ(matrix<FF>(2, 2, ffvec(7, {6, 4, 5, 0})), a + a);
-
-  WATCH(a);
-  WATCH(a * a);
+  EXPECT_EQ(matrix<FF>(2, 2, ffvec(7, {0, 6, 4, 5})), a * a);
 }
