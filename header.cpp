ed2cbed19544a9704669c4bb013c80baccc230b5
Chaoxu Tong <blackchaoxu@gmail.com>
2015-03-01 00:00:17 -0500

    fix bug of container watch

--- header.cpp
+++ header.cpp
@@ -4,6 +4,7 @@
 #define REP(i, n) for (int i = 0; i < (n); i++)
 #define SZ(c) int((c).size())
 #define ALL(c) (c).begin(), (c).end()
+#define TRACE(x...) x
 using namespace std;
 using ll = long long;
 using vi = vector<int>;
@@ -19,14 +20,20 @@ inline void chmin(C& x, const D& a) {
   if (x > a) x = a;
 }
 
-// Debug
+// CUT begin
 #include <iostream>
 #include <cstdio>
-#define TRACE(x...) x
 #define PRINT(x...) TRACE(fprintf(stderr, x))
 #define WATCH(x) TRACE({auto _VAL = (x); cerr << #x << " = " << _VAL << endl;})
 
-template<class C, class=typename C::iterator>
+template<class C>
+struct is_container {
+  using type = void;
+};
+
+template<> struct is_container<string> {};
+
+template<class C, class=typename is_container<C>::iterator>
 ostream& operator<<(ostream& out, const C& v) {
   for (auto x : v) out << x << ' ';
   return out;
@@ -37,3 +44,4 @@ ostream& operator<<(ostream& out, const pair<K,V>& v) {
   out << '(' << v.first << ',' << v.second << ')';
   return out;
 }
+// CUT end
