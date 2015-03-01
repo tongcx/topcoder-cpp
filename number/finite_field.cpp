a14b96557351e610f8dcbab307d67bba948a9671
Chaoxu Tong <blackchaoxu@gmail.com>
2015-03-01 08:31:54 -0500

    removed dft

--- number/finite_field.cpp
+++ number/finite_field.cpp
@@ -1,46 +1,18 @@
 #include <random>
 #include "number.cpp"
-using namespace std;
 
 struct FiniteField {
-  int p;
-  // a generator
-  int g;
-
   struct element {
-    FiniteField &ff;
+    FiniteField const &ff;
     int x;
 
-    element(int x, FiniteField &ff) : x(x), ff(ff) {}
+    element(int x, const FiniteField &ff) : x(x), ff(ff) {}
   };
 
+  int p;
 
-  FiniteField(int p) : p(p), g(generator()) {}
-  element operator[](int x) { return element(mod(x, p), *this);}
-
-
-  int generator() {
-    // return a generator of Zp
-    default_random_engine gen;
-    uniform_int_distribution<int> unif(1, p-1);
-
-    // factorization of p-1
-    vi fac, tmp;
-    factorize(p - 1, fac, tmp);
-
-    while(true) {
-      int g = unif(gen);
-      bool flag = true;
-
-      for (int q : fac)
-        if (pow(g, (p - 1) / q) == 1) {
-          flag = false;
-          break;
-        }
-
-      if (flag) return g;
-    }
-  }
+  FiniteField(int p) : p(p) {}
+  element operator[](int x) const { return element(mod(x, p), *this);}
 };
 
 FiniteField::element operator+(
@@ -67,6 +39,12 @@ FiniteField::element operator/(
   return a * b.ff[inv(b.x, b.ff.p)];
 }
 
+bool operator==(
+    const FiniteField::element &a,
+    const FiniteField::element &b) {
+  return a.x == b.x;
+}
+
 FiniteField::element pow(const FiniteField::element &n, int k) {
   if (k == 0) return n.ff[1];
   return pow(n*n, k / 2) * (k % 2 ? n : n.ff[1]);
@@ -74,4 +52,28 @@ FiniteField::element pow(const FiniteField::element &n, int k) {
 
 ostream& operator<<(ostream &out, const FiniteField::element &n) {
   out << n.x;
+}
+
+
+FiniteField::element find_generator(FiniteField &ff) {
+  // return a generator of Zp
+  default_random_engine gen;
+  uniform_int_distribution<int> unif(1, ff.p-1);
+
+  // factorization of p-1
+  vi fac, tmp;
+  factorize(ff.p - 1, fac, tmp);
+
+  while(true) {
+    FiniteField::element g = ff[unif(gen)];
+    bool flag = true;
+
+    for (int q : fac)
+      if (pow(g, (ff.p - 1) / q) == ff[1]) {
+        flag = false;
+        break;
+      }
+
+    if (flag) return g;
+  }
 }
